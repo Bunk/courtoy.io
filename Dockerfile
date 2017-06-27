fadcf4daf99b69fa417b349ffa034109af075f46
JD Courtoy <jd.courtoy@gmail.com>
2017-06-26 21:19:31 -0500

    Fixing proxy redirects

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM abiosoft/caddy
 MAINTAINER JD Courtoy <jd@courtoy.io>
 
 ENV CADDY_FILE /etc/Caddyfile
+COPY Caddyfile /etc/Caddyfile
 COPY entry.sh /
 RUN chmod +x /entry.sh
 
--- entry.sh
+++ entry.sh
@@ -2,21 +2,6 @@
 set -e
 
 # Generate the Caddyfile based on the following heredoc template
-search=`cat /etc/resolv.conf | head -n 1 | cut -d' ' -f2`
-cat >${CADDY_FILE} <<EOF
-jd.courtoy.io {
-    proxy /blog proxy.blog.${search} {
-        transparent
-    }
-
-    proxy /resume web.resume.${search}:8080 {
-        transparent
-    }
-    
-    expvar /stats
-
-    tls jd@courtoy.io
-}
-EOF
+export DNS_SEARCH=`cat /etc/resolv.conf | head -n 1 | cut -d' ' -f2`
 
 exec $@
